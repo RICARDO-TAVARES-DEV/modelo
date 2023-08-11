6b08bbd8c531c2c4f8ab980ce8078e4ac4cb1a68
Ricardo Tavares <ricotavares7719@gmail.com>
2023-08-11 11:04:52 -0300

    atual

---
+++ script.js
@@ -0,0 +1,143 @@
+console.log("Olá Mundo !!!")
+console.log('Oi "Tudo bem"')
+
+ let nome="Ricardo da Silva Tavares";
+// console.log(nome);
+ let comida="Churrasco"
+//let = variavel; o sinal = atribuição da variavel e nome é o valor da string
+//variavel + atribuição + valor
+//templateString só usa crase ``
+//Camel Case utilizar exemplo:bancoItau
+//console.log(typeof + variavel)
+//Variaveis são aramazens de dados
+//Variáveis podem ser recriadas significa 
+//que podemos atribuir um valor sem erro
+//Strings são dados textuais podem incuir letras números
+let n1=10;
+let n2=20;
+console.log(n1+n2);
+
+let frase= `A comida favorita do ${nome} é ${comida}` ;
+console.log(frase);
+console.log(typeof frase);
+
+let idade=46;
+console.log(typeof idade);
+
+// let type=null;
+// let type=undefined;
+// let type=NaN;
+// let type=Boolean;
+
+// console.log("*** Variáveis e condicionais" );
+// var cor="azul";
+// cor = "green";
+// console.log(cor);
+nome = "Ricardo Tavares";
+cor = "red";
+console.log(nome);
+console.log(cor);
+
+const maiorDeIdade = true;
+console.log(maiorDeIdade);
+console.log(typeof maiorDeIdade);// comm o typeof verificamos o tipo de dados
+// maiorDeIdade = false;quando declaramos uma const naõ podemos alterar o seu valor.
+// tipos:Boolean,Null,Undefined,Number,BigInt,String e Symbol.
+
+let nomeAluno="Ricardo";
+let profissao="Programador";
+let qualidade="òtimo"
+
+console.log(`O ${nomeAluno} é um ${qualidade} ${profissao} `)
+
+// //variaveis são onde armazenamos os dados
+// // variavel + nome da variavel + dado que quero guardar
+// var cor = "azul";
+// cor = "pink";
+// console.log(cor);
+
+// //variaveis podem ser recriadas, significando que podemos
+// // atribuir um novo valor, sem erro.
+
+// //let é a variavel preferida dos programadores
+// let nome = "Maria";
+// nome = "Grazy";
+// nome = "Murilo";
+// console.log(nome);
+
+// // constante não pode mudar
+// const maiorDeIdade = false;
+// // maiorDeIdade = false; não podemos reatribuir um valor
+// console.log(maiorDeIdade);
+// //Para verificar um tipo de dado usamos o typeof
+
+// //Nomes para nossas variaveis:
+// // não pode ter espaços
+// // usamos o camel case = camelCase
+// // não usamos simbolos e numeros exceto $ e _
+
+// let turmaT1 = "maravilhosa";
+// console.log(typeof turmaT1);
+
+// const nomeAluno = "Denis";
+// let comidaFavorita = "Lasanha";
+// console.log(`Meu aluno ${nomeAluno} gosta de ${comidaFavorita}`);
+// // console.log("oi meu nome é ${nomeAluno}"); isso é um texto
+
+//TemplateString `${}`
+
+//tipo de dados:
+// String dados textuais "", ''
+
+//Boleano retorna false ou true, verdadeiro ou falso; (boolean)
+
+//null é dado nulo, inexistente;
+
+// //indefinido (undefined)
+// let escola;
+// console.log(escola);
+
+// //NaN dado não numerico
+// let teste = "cem" / 2;
+// console.log(teste);
+
+// // if(condição){
+// //   codigo será executado se for verdadeiro
+// // } else{
+// //   senão, executa esse codigo
+// // }
+
+// let brasileiro = false;
+
+// if (brasileiro) {
+//   console.log("Que lindo! Você é brasileiro!");
+// } else {
+//   console.log("Nao hablo sua lingua!");
+// }
+
+//EXERCÍCIOS PARA TREINAR 
+// Declarar uma variável com o seu nome
+
+// Declarar uma variável com a sua idade
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+// Atribuir valor a sua comida favorita
+
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
+
+
+
+
